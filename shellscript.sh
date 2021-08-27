4355326753c67f5956856b0dfd5dbc5a198fda76
jyopgdba <89170230+jyopgdba@users.noreply.github.com>
2021-08-27 03:34:46 -0700

    Revert "jyogittes: c4 - Adding for loop (#4)"
    This reverts commit f3f2a2e6cac021b07d28ddfd307d89e7b9bf2e00.

--- shellscript.sh
+++ shellscript.sh
@@ -8,12 +8,6 @@ mul=$(( a*b ))
 echo "Multiply $a * $b = $mul" 
 ans=$(( a + b))
 echo "SUM of $a + $b = $ans"
-START=1
-END=5
-for i in {$START..$END}
-do
-   echo "$i"
-done
 valid=true
 count=1
 while [ $valid ]
